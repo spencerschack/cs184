af6b035c98ada832aa440a89a426932e9bd9b256
Spencer Schack <scs1818@gmail.com>
2014-03-10 15:21:40 -0700

    moved lib to vendor and use lib as our dir

---
+++ hw2/lib/options_parser.cpp
@@ -0,0 +1,6 @@
+void parse_options(int argc, char* argv[]) {
+	int i;
+	for(i = 0; i < argc; i++) {
+		printf("%s\n", argv[i]);
+	}
+}
--- hw2/ray_tracer.cpp
+++ hw2/ray_tracer.cpp
@@ -1,5 +1,16 @@
-#include "lib/Eigen/Eigen"
+// Standard libraries
+#include <stdio.h>
+
+// Third-party libraries
+#include "vendor/Eigen/Eigen"
+
+// Our classes
+#include "lib/options_parser.cpp"
+
+using namespace Eigen;
+using namespace std;
 
 int main(int argc, char* argv[]) {
+	parse_options(argc, argv);
 	return 0;
 }
