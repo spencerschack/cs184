2aee0ad9faa74984c9e79234d86d6775fe9ddd13
Spencer Schack <scs1818@gmail.com>
2014-04-10 18:09:57 -0700

    started writing subdiv routine

--- hw3/bezier.cpp
+++ hw3/bezier.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <cmath>
+#include <cfloat>
 
 #include <sys/time.h>
 
@@ -24,64 +25,14 @@ inline float sqr(float x) { return x*x; }
 
 using namespace std;
 
-class Viewport {
-public:
-  int w, h;
-};
-
-class Printable {
-public:
-  virtual string inspect() const = 0;
-  void print() const {
-    cout << inspect() << '\n';
-  }
-};
-
-class Point : public Printable {
-public:
-  float x, y, z;
-  Point(float x, float y, float z) : x(x), y(y), z(z) { }
-  string inspect() const {
-    stringstream str;
-    str << "Point<"
-        << "x: " << x << ", "
-        << "y: " << y << ", "
-        << "z: " << z << ">";
-    return str.str();
-  }
-};
-
-class Curve : public Printable {
-public:
-  Point p0, p1, p2, p3;
-  Curve(Point p0, Point p1, Point p2, Point p3) :
-    p0(p0), p1(p1), p2(p2), p3(p3) { };
-  string inspect() const {
-    stringstream str;
-    str << "Curve<"
-        << "p0: " << p0.inspect() << ", "
-        << "p1: " << p1.inspect() << ", "
-        << "p2: " << p2.inspect() << ", "
-        << "p3: " << p3.inspect() << ">";
-    return str.str();
-  }
-};
-
-class Patch : public Printable {
-public:
-  Curve c0, c1, c2, c3;
-  Patch(Curve c0, Curve c1, Curve c2, Curve c3) :
-    c0(c0), c1(c1), c2(c2), c3(c3) { }
-  string inspect() const {
-    stringstream str;
-    str << "Patch<"
-        << "c0: " << c0.inspect() << ", "
-        << "c1: " << c1.inspect() << ", "
-        << "c2: " << c2.inspect() << ", "
-        << "c3: " << c3.inspect() << ">";
-    return str.str();
-  }
-};
+class Viewport;
+class Printable;
+class Point;
+class PointNormal;
+class PointDerivative;
+class Quad;
+class Curve;
+class Patch;
 
 typedef enum {
   UniformTesselation,
@@ -98,15 +49,154 @@ typedef enum {
   WireframeRendering
 } RenderingMode;
 
+class Viewport {
+public:
+  int w, h;
+};
+
 Viewport viewport;
 float subdivisionParameter;
 TesselationMode tesselationMode = UniformTesselation;
 ShadingMode shadingMode = SmoothShading;
 RenderingMode renderingMode = FilledRendering;
 vector<Patch> patches;
+vector<Quad> quads;
+
+class Printable {
+public:
+  virtual string inspect() const = 0;
+  void print() const {
+    cout << inspect() << '\n';
+  }
+};
+
+class Point : public Printable {
+public:
+  float x, y, z;
+  Point() { }
+  Point(float x, float y, float z) : x(x), y(y), z(z) { }
+  string inspect() const {
+    stringstream str;
+    str << "Point<"
+        << "x: " << x << ", "
+        << "y: " << y << ", "
+        << "z: " << z << ">";
+    return str.str();
+  }
+};
+
+class PointNormal : public Printable {
+public:
+  Point point, normal;
+  PointNormal() { }
+  PointNormal(Point point, Point normal) : point(point), normal(normal) { }
+  string inspect() const {
+    stringstream str;
+    str << "PointNormal<"
+        << "point: " << point.inspect() << ", "
+        << "normal: " << normal.inspect() << ">";
+    return str.str();
+  }
+};
+
+class PointDerivative : public Printable {
+public:
+  Point point, derivative;
+  PointDerivative() { }
+  PointDerivative(Point point, Point derivative) :
+    point(point), derivative(derivative) { }
+  string inspect() const {
+    stringstream str;
+    str << "PointDerivative<"
+        << "point: " << point.inspect() << ", "
+        << "derivative: " << derivative.inspect() << ">";
+    return str.str();
+  }
+};
+
+class Quad : public Printable {
+public:
+  Point p0, p1, p2, p3;
+  Quad() { }
+  Quad(Point p0, Point p1, Point p2, Point p3) :
+    p0(p0), p1(p1), p2(p2), p3(p3) { }
+  string inspect() const {
+    stringstream str;
+    str << "Quad<"
+        << "p0: " << p0.inspect() << ", "
+        << "p1: " << p1.inspect() << ", "
+        << "p2: " << p2.inspect() << ", "
+        << "p3: " << p3.inspect() << ">";
+    return str.str();
+  }
+};
+
+class Curve : public Printable {
+public:
+  Point p0, p1, p2, p3;
+  Curve() { }
+  Curve(Point p0, Point p1, Point p2, Point p3) :
+    p0(p0), p1(p1), p2(p2), p3(p3) { };
+  string inspect() const {
+    stringstream str;
+    str << "Curve<"
+        << "p0: " << p0.inspect() << ", "
+        << "p1: " << p1.inspect() << ", "
+        << "p2: " << p2.inspect() << ", "
+        << "p3: " << p3.inspect() << ">";
+    return str.str();
+  }
+  Point interpolate(float u) const {
+
+  }
+};
+
+class Patch : public Printable {
+public:
+  Curve c0, c1, c2, c3;
+  Patch() { }
+  Patch(Curve c0, Curve c1, Curve c2, Curve c3) :
+    c0(c0), c1(c1), c2(c2), c3(c3) { }
+  string inspect() const {
+    stringstream str;
+    str << "Patch<"
+        << "c0: " << c0.inspect() << ", "
+        << "c1: " << c1.inspect() << ", "
+        << "c2: " << c2.inspect() << ", "
+        << "c3: " << c3.inspect() << ">";
+    return str.str();
+  }
+  void subdivide() const {
+    float numDivs = (1.0f + FLT_EPSILON) / subdivisionParameter;
+    float u, v;
+    Point lastFrontPoint, lastBackPoint, currentFrontPoint, currentBackPoint;
+    Curve currentCurve, lastCurve = interpolate(0);
+    for(u = subdivisionParameter; u < 1.0; u += subdivisionParameter) {
+      currentCurve = interpolate(u);
+      lastBackPoint = lastCurve.interpolate(0);
+      lastFrontPoint = currentCurve.interpolate(0);
+      for(v = subdivisionParameter; v < 1.0; v += subdivisionParameter) {
+        currentFrontPoint = currentCurve.interpolate(v);
+        currentBackPoint = lastCurve.interpolate(v);
+        quads.push_back(Quad(lastBackPoint, currentBackPoint,
+          currentFrontPoint, lastFrontPoint));
+        lastFrontPoint = currentFrontPoint;
+        lastBackPoint = currentBackPoint;
+      }
+      lastCurve = currentCurve;
+    }
+  }
+  Curve interpolate(float u) const {
+    return Curve(c0.interpolate(u), c1.interpolate(u),
+      c2.interpolate(u), c3.interpolate(u));
+  }
+};
 
 void initScene(){
-
+  Patch patch;
+  for(vector<Patch>::iterator p = patches.begin(); p != patches.end(); ++p) {
+    p->subdivide();
+  }
 }
 
 void reshape(int w, int h) {
@@ -181,7 +271,7 @@ void display() {
   glutSwapBuffers();
 }
 
-void keyboard(unsigned char key, int x, int y) {
+void keyPress(unsigned char key, int x, int y) {
   switch(key) {
     case 's': {
       shadingMode = shadingMode == SmoothShading ? FlatShading : SmoothShading;
@@ -191,6 +281,10 @@ void keyboard(unsigned char key, int x, int y) {
       break;
     }
   }
+}
+
+void specialPress(int key, int x, int y) {
+
 }
 
 Point parsePoint(ifstream &file) {
@@ -251,7 +345,8 @@ int main(int argc, char *argv[]) {
   initScene();
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
-  glutKeyboardFunc(keyboard);
+  glutKeyboardFunc(keyPress);
+  glutSpecialFunc(specialPress);
   glutMainLoop();
   return 0;
 }
